e4000701961de0b146bd4bda77403a9d9d4eda13
sayantan <123sayantandas@gmail.com>
2024-06-21 00:11:56 +0530

    delete user

--- server/routes/userProfile.js
+++ server/routes/userProfile.js
@@ -3,8 +3,16 @@ const User = require("../db/userModel");
 const adoptPost = require("../db/AdoptPost");
 const verifyToken = require("../utils/verifyToken");
 const jwt = require("jsonwebtoken");
+const cloudinary = require("cloudinary");
 
 const router = express.Router();
+
+cloudinary.config({
+  cloud_name: "dkf5lwjqr",
+  api_key: "217799987144445",
+  api_secret: "dr6vZEhZ4uU07PxCayir2ZbSFz4",
+});
+
 
 router.get("/getuser", verifyToken, (req, res) => {
   try {
@@ -25,7 +33,7 @@ router.get("/getuser", verifyToken, (req, res) => {
       res.status(200).json({user: user, adopt: alladoptPosts});
     });
   } catch (err) {
-    console.log(err);
+    // console.log(err);
     res.status(400).json("User Not Found")
   }
 });
@@ -72,5 +80,43 @@ router.patch("/edit", verifyToken, (req, res) => {
     res.status(400).json(err);
   }
 });
+
+router.delete('/deleteUser', verifyToken, (req, res)=>{
+  try{
+    jwt.verify(req.token, "shhh", async (err, dataa) => {
+      if (err) {
+        res.status(403);
+      }
+      const user = await User.findOne({ _id: dataa.id });
+      if (!user) {
+        console.log("user not found");
+        res.status(400).json("Invalid User");
+      }
+      // console.log(user);
+      const image_id = user.imageID;
+
+      try {
+        cloudinary.v2.uploader.destroy(image_id).then((result) => {
+          if (result) {
+            console.log(result);
+            console.log("image deleted from cloudinary")
+            //return res.status(201).json({message:'Image Deleted From Cloudinary'});
+          }
+        });
+      } catch (error) {
+        console.log(error);
+        return res.send({ error: "error" });
+      }
+
+      const data = await User.findByIdAndDelete(user._id);
+      console.log(data)
+      res.status(200).json("image and account deleted successfully")
+
+    })
+  }catch(err){
+    console.log(err)
+    res.status(400).json(err)
+  }
+})
 
 module.exports = router;
